a7f1285cbfdabfc06670f7ecff96ae82a50d7b27
Yanay1 <yanayrosen@berkeley.edu>
2021-06-22 12:48:28 -0700

    hw01 test fix

---
+++ materials/su21/hw/hw01/tests/q8_1.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q8_1',
+    'points': [1],
+    'suites': [{'cases': [{'code': '>>> survey == "feeling datagr8"\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
