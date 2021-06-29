387c6ebfae0cf7c86ee0538805992d43deaa0790
Yanay1 <yanayrosen@berkeley.edu>
2021-06-29 11:56:03 -0700

    hw03 release

---
+++ materials/su21/hw/hw03/tests/q2_4.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q2_4',
+    'points': [0],
+    'suites': [{'cases': [{'code': '>>> 0 <= less_than_west_births <= 52\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ materials/su21/hw/hw03/tests/q2_7.py
@@ -0,0 +1,1 @@
+test = {'name': 'q2_7', 'points': [0], 'suites': [{'cases': [{'code': '>>> type(assoc) is bool\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
