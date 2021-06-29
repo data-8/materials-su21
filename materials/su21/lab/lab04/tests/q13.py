648cbf25f010951a9c2dccffa336b380b8d0b9b1
Yanay1 <yanayrosen@berkeley.edu>
2021-06-28 19:12:50 -0700

    lab04 release

---
+++ materials/su21/lab/lab04/tests/q13.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q13',
+    'points': None,
+    'suites': [{'cases': [{'code': '>>> disemvowel("Datascience rules!") == "Dtscnc rls!"\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ materials/su21/lab/lab04/tests/q613.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q613',
+    'points': None,
+    'suites': [   {   'cases': [{'code': '>>> import math\n>>> math.isclose(average_pay_2014, 11649176.115603436, rel_tol = 0.01)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
