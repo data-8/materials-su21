af0c62d8bcc9dd974727a4dabef4e4d774b4f92e
Yanay1 <yanayrosen@berkeley.edu>
2021-07-06 12:27:23 -0700

    release hw05

---
+++ materials/su21/hw/hw05/tests/q1_5.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q1_5',
+    'points': [5, 5],
+    'suites': [   {   'cases': [{'code': '>>> is_multiple_6_or_8(24)\nTrue', 'hidden': False, 'locked': False}, {'code': '>>> type(some_string) == str\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
