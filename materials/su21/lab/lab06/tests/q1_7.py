e2bcbe42fb4fa817351599fd4bc76a4f6766db0b
Yanay1 <yanayrosen@berkeley.edu>
2021-07-10 22:33:15 -0700

    lab06 release

---
+++ materials/su21/lab/lab06/tests/q1_7.py
@@ -0,0 +1,11 @@
+test = {   'name': 'q1_7',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': '>>> len(model_proportions) % 2 == 0\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> len(np.unique(model_proportions)) == 1\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> sum(model_proportions) == 1\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> type(simulation_proportion_correct) == float\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> 0 <= one_statistic <= 25\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
